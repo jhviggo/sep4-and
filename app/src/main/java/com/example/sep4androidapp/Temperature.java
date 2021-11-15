a9fe9094f7d57dc8b8e5cbba4162149aabdbba60
peter <petertamas05@gmail.com>
2021-11-15 20:52:57 +0100

    Initial App Layout Commit

---
+++ app/src/main/java/com/example/sep4androidapp/Temperature.java
@@ -0,0 +1,15 @@
+package com.example.sep4androidapp;
+
+public class Temperature {
+    private String value;
+
+    public Temperature(String value)
+    {
+        this.value = value;
+    }
+
+    public String getData()
+    {
+        return value;
+    }
+}
